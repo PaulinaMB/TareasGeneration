f23c97fbde1f9219365aaf1b03a031188b6137e4
PaulinaMB <paulinamendozabernal@gmail.com>
2023-12-08 07:45:04 -0600

    (+) Actividades realizadas en clase y tareas. Se realizaron cambios en los nombres de las carpetas. Se eliminaran los repositorios hechos para cada una de las prácticas y  estarán dentro de este.

---
+++ Javascript/JavaScript/script.js
@@ -0,0 +1,18 @@
+//comentarios de una sola línea
+/*comentarios
+multilinea */
+//alert("Hola mundo");
+
+//console.log("Este es un mensaje para mi consola");
+
+// var nombre=prompt("Ingresa tu nombre");
+// alert("Hola, "+ nombre +" Bienvenido a la ch35");
+
+// var numero1 = parseInt (prompt("Ingresa el primer número:"));
+// var numero2 = parseInt (prompt("Ingresa el segundo número:"));
+
+// console.log("suma:" + (numero1 + numero2));
+// console.log("resta:" + (numero1 - numero2));
+// console.log("multiplicación:" + (numero1 * numero2));
+// console.log("división:" + (numero1 / numero2));
+
